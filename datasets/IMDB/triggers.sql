8d3b4e39ecfd92054f77bb2661baccd0c21f6859
Faizan Haider <faizanhaider@Faizans-MacBook-Air.local>
2023-11-22 13:40:06 +0530

    Repaired triggers

--- datasets/IMDB/triggers.sql
+++ datasets/IMDB/triggers.sql
@@ -1,30 +1,43 @@
 -- Trigger for INSERT operation on IMDB_title_basics
+use iia_project;
+
+DELIMITER //
 CREATE TRIGGER imdb_title_basics_insert_trigger
 AFTER INSERT ON IMDB_title_basics
 FOR EACH ROW
 BEGIN
-    -- Insert the new data into the GLOBAL_MOVIE_GENRE view
     INSERT INTO GLOBAL_MOVIE_GENRE (MovieID, Title, Genre)
     VALUES (NEW.tconst, NEW.primaryTitle, NEW.genres);
 END;
+//
+DELIMITER ;
+
 
 -- Trigger for UPDATE operation on IMDB_title_basics
+DELIMITER //
 CREATE TRIGGER imdb_title_basics_update_trigger
 AFTER UPDATE ON IMDB_title_basics
 FOR EACH ROW
 BEGIN
-    -- Update the corresponding data in the GLOBAL_MOVIE_GENRE view
-    UPDATE GLOBAL_MOVIE_GENRE
-    SET Title = NEW.primaryTitle, Genre = NEW.genres
-    WHERE MovieID = NEW.tconst;
+    UPDATE GLOBAL_MOVIE_GENRE GMG
+    JOIN IMDB_title_basics IMDb
+    ON GMG.MovieID = IMDb.tconst
+    SET GMG.Title = NEW.primaryTitle,
+        GMG.Genre = NEW.genres;
 END;
+//
+DELIMITER ;
+
 
 -- Trigger for DELETE operation on IMDB_title_basics
+
+DELIMITER //
 CREATE TRIGGER imdb_title_basics_delete_trigger
 AFTER DELETE ON IMDB_title_basics
 FOR EACH ROW
-BEGIN
-    -- Delete the corresponding data from the GLOBAL_MOVIE_GENRE view
     DELETE FROM GLOBAL_MOVIE_GENRE
     WHERE MovieID = OLD.tconst;
-END;
+DELIMITER ;
+-- Reset the delimiter back to ;
+
+
--- datasets/TMDB/triggers.sql
+++ datasets/TMDB/triggers.sql
@@ -1,24 +1,26 @@
 -- Trigger for INSERT operation on TMDB_MOVIE
 -- Trigger for INSERT operation on TMDB_MOVIE
+Delimiter //
 CREATE TRIGGER tmdb_movie_insert_trigger
 AFTER INSERT ON TMDB_MOVIE
 FOR EACH ROW
 BEGIN
-    -- Insert the new data into the GLOBAL_MOVIE_GENRE view
     INSERT INTO GLOBAL_MOVIE_GENRE (MovieID, Title, Genre)
     SELECT NEW.ID, NEW.TITLE, TG.GENRE
     FROM TMDB_MOVIE_GENRE TMG
     JOIN TMDB_GENRE TG ON TMG.GENREID = TG.ID
     WHERE TMG.FILMID = NEW.FILMID;
 END;
+//
+Delimiter ;
 
 
 -- Trigger for UPDATE operation on TMDB_MOVIE
+Delimiter //
 CREATE TRIGGER tmdb_movie_update_trigger
 AFTER UPDATE ON TMDB_MOVIE
 FOR EACH ROW
 BEGIN
-    -- Update the corresponding data in the GLOBAL_MOVIE_GENRE view
     UPDATE GLOBAL_MOVIE_GENRE GMG
     SET GMG.Title = NEW.TITLE,
         GMG.Genre = (
@@ -29,14 +31,18 @@ BEGIN
         )
     WHERE GMG.MovieID = NEW.ID;
 END;
+//
+Delimiter ;
 
 
 -- Trigger for DELETE operation on TMDB_MOVIE
+Delimiter //
 CREATE TRIGGER tmdb_movie_delete_trigger
 AFTER DELETE ON TMDB_MOVIE
 FOR EACH ROW
 BEGIN
-    -- Delete the corresponding data from the GLOBAL_MOVIE_GENRE view
     DELETE FROM GLOBAL_MOVIE_GENRE
     WHERE MovieID = OLD.ID;
 END;
+//
+Delimiter ;
